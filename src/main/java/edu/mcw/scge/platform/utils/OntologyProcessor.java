6e8806a671b12f6d6d175789c66a6708c80bd6f6
Thota <jthota@mcw.edu>
2025-04-25 13:58:28 -0500

    updated doids

--- src/main/java/edu/mcw/scge/platform/utils/OntologyProcessor.java
+++ src/main/java/edu/mcw/scge/platform/utils/OntologyProcessor.java
@@ -9,6 +9,8 @@ import com.google.gson.Gson;
 import edu.mcw.scge.dao.implementation.ClinicalTrailDAO;
 import edu.mcw.scge.datamodel.ClinicalTrialAdditionalInfo;
 import edu.mcw.scge.datamodel.ClinicalTrialRecord;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.client.RestTemplate;
 ;
 import java.util.List;
@@ -17,42 +19,28 @@ import java.util.Map;
 public class OntologyProcessor {
 //    Gson gson=new Gson();
     ClinicalTrailDAO clinicalTrailDAO=new ClinicalTrailDAO();
+    public Logger logger = LogManager.getLogger("ontology");
     public void uploadParentTerms() throws Exception {
         List<ClinicalTrialRecord> records = clinicalTrailDAO.getAllClinicalTrailRecords();
         String baseURI = "https://rest.rgd.mcw.edu/rgdws/ontology/termAndParentTermWithSynonyms/";
         RestTemplate restTemplate=new RestTemplate();
         restTemplate.setErrorHandler(new ApiResponseErrorHandler());
-        ObjectMapper mapper = new ObjectMapper();
         for (ClinicalTrialRecord record : records) {
             String ontId=record.getIndicationDOID();
             if (ontId != null && !ontId.equals("")) {
-                int id=0;
-                try{
-                  id=  Integer.parseInt(ontId);
-                }catch (Exception e){
-                    System.out.println("ERROR DOID:"+ ontId);
-                }
-                if(id>0) {
-                    String fetchURI = baseURI + "DOID:" + ontId;
+                for(String id:ontId.split(",")) {
+                    String fetchURI = baseURI + id;
                     try {
                         List response = restTemplate.getForObject(fetchURI, List.class);
-//                    System.out.println("res"+ response);
                         if (response != null && response.size() > 0) {
                             for (Object synonym : response) {
                                 uploadInfoObject(synonym.toString(), record.getNctId());
                             }
                         }
 
-//                    if (response != null && response.length() > 2) {
-//                       List synonyms = mapper.readValue(response, List.class);
-////                        System.out.println("ID MAP:" + gson.toJson(idMap));
-//                        for(Object synonym:synonyms){
-//                            uploadInfoObject(synonym.toString(), record.getNctId());
-//
-//                        }
-//                    }
                     } catch (Exception e) {
                         e.printStackTrace();
+                        logger.info("ERROR ONT ID:"+id);
                     }
                 }
             }
